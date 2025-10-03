2db6b202b31155c5dbf230d4492ba2776b18a1f5
Khin Su Wai <khinsu@xenoptics.com>
2025-10-03 10:24:16 +0700

    added unit test

--- src/__test__/Pizza.test.tsx
+++ src/__test__/Pizza.test.tsx
@@ -1,2 +1,23 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
+import Pizza from "../Pizza";
+afterEach(cleanup);
+test("alt text render on image", async () => {
+  const name = "My favorite pizza";
+  const src = "https://picsum.photos/200";
+  const screen = render(
+    <Pizza name={name} description={"super cool pizza"} image={src} />
+  );
+
+  const img = screen.getByRole("img") as HTMLImageElement;
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
+
+test("ot have default image if none is provided", () => {
+  const screen = render(
+    <Pizza name={"Cool Pizza"} description={"super cool pizza"} />
+  );
+  const img = screen.getByRole("img") as HTMLImageElement;
+  expect(img.src).not.toBe("");
+});
---
+++ src/__test__/usePizzaOfTheDay.test.tsx
@@ -0,0 +1,52 @@
+import { expect, test, vi } from "vitest";
+import { render } from "@testing-library/react";
+import createFetchMock from "vitest-fetch-mock";
+import usePizzaOfTheDay from "../hooks/usePizzaOfTheDay";
+import { renderHook, waitFor } from "@testing-library/react";
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+const testPizza = {
+  id: "calabrese",
+  name: "The Calabrese Pizza",
+  category: "Supreme",
+  description:
+    "Salami, Pancetta, Tomatoes, Red Onions, Friggitello Peppers, Garlic",
+  image: "/public/pizzas/calabrese.webp",
+  sizes: { S: 12.25, M: 16.25, L: 20.25 },
+};
+
+function getPizzaOfTheDay() {
+  let pizza;
+
+  function TestComponent() {
+    pizza = usePizzaOfTheDay();
+    return null;
+  }
+  render(<TestComponent />);
+
+  return pizza;
+}
+
+// test("give null when first called", async () => {
+//   fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
+//   const pizza = getPizzaOfTheDay();
+//   expect(pizza).toBeNull();
+// });
+
+test("to be null on initial load", async () => {
+  fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay("lol"));
+  expect(result.current).toBeNull();
+});
+
+test("to call the API and give back the pizza of the day", async () => {
+  fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay());
+  await waitFor(() => {
+    expect(result.current).toEqual(testPizza);
+  });
+
+  expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
+});
